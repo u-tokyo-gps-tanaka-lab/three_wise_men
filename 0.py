76a86afeeb482048481e1bc8f86bea1afd424683
tanakat01 <ktanaka+github@tanaka.ecc.u-tokyo.ac.jp>
2018-11-26 18:06:55 +0900

    Update 0.py

--- 0.py
+++ 0.py
@@ -3,7 +3,7 @@ import itertools
 for n in range(10):
     solver = Solver()
     vs = [Int("a"), Int("b"), Int("c")]
-    solver.add([x != y for x, y in itertools.combinations(v, 2)])
+    solver.add([x != y for x, y in itertools.combinations(vs, 2)])
     solver.add([And(1 <= x, x <= n) for x in vs])
     if solver.check() == sat:
         print("n = %d" % n)
